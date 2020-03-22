509c60a4c41507ddc688311f046a9dcb1d76a5a3
Jet Martin <jetamartin@gmail.com>
2020-03-22 12:22:55 -0700

    basic functionality working..next will be to refactor and refine

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Setup config
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash, session, jsonify
 # from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -10,14 +10,36 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 from boggle import Boggle
 boggle_game = Boggle()
 
+
 @app.route('/')
 def root():
-  if (session['game_board']):
-    game_board = session['game_board']
-  else:
-   game_board = boggle_game.make_board()
-  return render_template('game_board.html', game_board = game_board)
 
-@app.route('/word_guess')
+  game_board = boggle_game.make_board()
+  session['game_board'] = game_board
+  high_score = session.get("high_score", 0)
+  play_count = session.get("play_count", 0)
+  return render_template('game_board.html', game_board = game_board, 
+  high_score = high_score, play_count = play_count)
+
+@app.route('/word_check')
 def word_submit():
-  return render_template('')
+ 
+  game_board = session['game_board']
+  word_value = request.args['word']
+  word_check = boggle_game.check_valid_word(game_board, word_value)
+  # import pdb; pdb.set_trace()
+  check_results = {'result' : word_check}
+  return jsonify(check_results)
+
+@app.route('/post_score', methods = ['POST'])
+def record_score():
+  # import pdb; pdb.set_trace()
+  # highest_score = request.json['params']['score']
+  score = request.json["score"]
+  high_score = session.get("high_score", 0)
+  play_count = session.get("play_count", 0)
+
+  session['play_count'] = play_count + 1
+  session['high_score'] = max(score, high_score)
+
+  return jsonify(newHighScore = score > high_score)
--- static/scripts.js
+++ static/scripts.js
@@ -1,11 +1,87 @@
 $(function() {
+  
+  let scoreCount = $('#score-count');
 
-  $("h1").on("click", function(evt) {
-    console.log("Hello");
-  })
+  score = 0;
 
-  $("#word_form").on("submit", function(evt) { 
+  $("#word-form").on("submit", handleSubmit.bind(this));
+  
+  // function incrementScore(word) {
+  //   console.log('increaseScore function');
+  //   score+= word.length;
+  //   console.log('Score = ', score);
+  // }
+
+  async function postScore ( score ) {
+    let response = await axios.post("http://127.0.0.1:5000/post_score", 
+    { "score": score } );
+    // console.log(`High Score = ${high_score}, Score = ${score}`)
+    console.log(response.data.newHighScore);
+    // if (response.data.newHighScore) {
+    //   score = high_score
+    // }
+    //   this.showMessage(`New record: ${this.score}`, "ok");
+    // } else {
+    //   this.showMessage(`Final score: ${this.score}`, "ok");
+    // }
+    // console.log(highscore);
+  }
+
+  async function handleSubmit(evt) { 
+    let wordInput = $("#word-input")
+    console.log("handleSubmit function")
     evt.preventDefault();
-    console.log("submitted");
-    })
+    let word = wordInput.val();
+    wordInput.val('');
+    
+    results = await axios.get("http://127.0.0.1:5000/word_check", 
+    { params: 
+      { word: word } 
+    } )
+    console.log(results); 
+    result_code = results.data.result;
+    
+    if (result_code == 'ok') {
+      result_msg = $('#results-msg').text(result_code)
+      score+= word.length;
+      scoreCount.text(score);
+      
+    }
+    else if (result_code == 'not-on-board') {
+      $('#results-msg').text(`Sorry '${word}' is not on the game board`)
+    }
+    else {
+      result_msg = $('#results-msg').text(result_code)
+    }
+  }
+
+    function startTimer(duration, display) {
+      var timer = duration, minutes, seconds;
+      let x = setInterval(function () {
+          minutes = parseInt(timer / 60, 10);
+          seconds = parseInt(timer % 60, 10);
+  
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+          display.text(minutes + ":" + seconds);
+  
+          if (--timer < 0) {
+              $('#timer-expired').toggle();
+              $('#timer').toggle();
+              $("#word-form :input").prop("disabled", true);
+              clearInterval(x);
+              console.log("score count", score);
+              postScore(score);
+              return;
+          }
+      }, 1000);
+  }
+  
+
+let sixtySeconds = 60,
+    display = $('#timer');
+startTimer(sixtySeconds, display);
+   
+
 })
