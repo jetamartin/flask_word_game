74dbc3678389606113ce8216e2a2efb470256b3a
Jet Martin <jetamartin@gmail.com>
2020-03-23 09:07:27 -0700

    refactored scripts.js to use classes - works but still issue when timer expires

--- static/scripts.js
+++ static/scripts.js
@@ -1,87 +1,94 @@
-$(function() {
-  
-  let scoreCount = $('#score-count');
+class BoggleGame {
+  constructor (gameDuration = 60) {
+    this.gameDuration = gameDuration;
 
-  score = 0;
+    this.score = 0;
+    this.wordAlreadyMatched = new Set(); 
 
-  $("#word-form").on("submit", handleSubmit.bind(this));
-  
-  // function incrementScore(word) {
-  //   console.log('increaseScore function');
-  //   score+= word.length;
-  //   console.log('Score = ', score);
-  // }
-
-  async function postScore ( score ) {
-    let response = await axios.post("http://127.0.0.1:5000/post_score", 
-    { "score": score } );
-    // console.log(`High Score = ${high_score}, Score = ${score}`)
-    console.log(response.data.newHighScore);
-    // if (response.data.newHighScore) {
-    //   score = high_score
-    // }
-    //   this.showMessage(`New record: ${this.score}`, "ok");
-    // } else {
-    //   this.showMessage(`Final score: ${this.score}`, "ok");
-    // }
-    // console.log(highscore);
+    this.scoreCount = $('#score-count');
+    $("#word-form").on("submit", this.handleSubmit.bind(this));
+       
   }
 
-  async function handleSubmit(evt) { 
+  async postScore( score ) {
+    let response = await axios.post("http://127.0.0.1:5000/post_score", 
+    { "score": score } );
+
+    if (response.data.newHighScore) {
+      this.showMessage(`New record: ${this.score}`, ".text-primary");
+    } else {
+      this.showMessage(`Final score: ${this.score}`, ".text-primary");
+    }
+  }
+
+  showMessage(msg, msgType) {
+    $('.results-msg')
+      .text(msg)
+      .removeClass()
+      .addClass(`results-msg ${msgType}`);
+  }
+
+  showMatchedWord(word) {
+    $("ul").append(`<li>${word}</li>`);
+  }
+  
+
+  async handleSubmit(evt) { 
+    let msg, msgType;
     let wordInput = $("#word-input")
-    console.log("handleSubmit function")
     evt.preventDefault();
     let word = wordInput.val();
-    wordInput.val('');
     
-    results = await axios.get("http://127.0.0.1:5000/word_check", 
+    let results = await axios.get("http://127.0.0.1:5000/word_check", 
     { params: 
       { word: word } 
     } )
-    console.log(results); 
-    result_code = results.data.result;
+    let result_code = results.data.result;
     
     if (result_code == 'ok') {
-      result_msg = $('#results-msg').text(result_code)
-      score+= word.length;
-      scoreCount.text(score);
-      
+      msgType = ".text-success";
+      msg = `Congrats! '${word}' matched`;
+      this.score+= word.length;
+      this.scoreCount.text(this.score);
+      this.showMessage(msg, msgType);
+      this.showMatchedWord(word);
+
+    } else if (result_code == 'not-on-board') {
+      const msg = `Sorry '${word}' is not on the game board. Try again`;
+      const msgType = ".text-danger";
+      this.showMessage(msg, msgType);
+    } else {
+      const msg = `Sorry '${word}' is not a word. Try again`;
+      const msgType = ".text-danger";
+      this.showMessage(msg, msgType);
     }
-    else if (result_code == 'not-on-board') {
-      $('#results-msg').text(`Sorry '${word}' is not on the game board`)
-    }
-    else {
-      result_msg = $('#results-msg').text(result_code)
-    }
+    wordInput.val('').focus();
   }
 
-    function startTimer(duration, display) {
-      var timer = duration, minutes, seconds;
-      let x = setInterval(function () {
-          minutes = parseInt(timer / 60, 10);
-          seconds = parseInt(timer % 60, 10);
-  
-          minutes = minutes < 10 ? "0" + minutes : minutes;
-          seconds = seconds < 10 ? "0" + seconds : seconds;
-  
-          display.text(minutes + ":" + seconds);
-  
-          if (--timer < 0) {
-              $('#timer-expired').toggle();
-              $('#timer').toggle();
-              $("#word-form :input").prop("disabled", true);
-              clearInterval(x);
-              console.log("score count", score);
-              postScore(score);
-              return;
-          }
-      }, 1000);
+  async startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    let x = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            $('#timer-expired').toggle();
+            $('#timer').toggle();
+            $("#word-form :input").prop("disabled", true);
+            clearInterval(x);
+            this.postScore(this.score);
+            return;
+        }
+    }, 1000);
   }
-  
-
-let sixtySeconds = 60,
-    display = $('#timer');
-startTimer(sixtySeconds, display);
-   
-
-})
+}
+ 
+  // Start the timer
+  // const display = $('#timer');
+  // this.startTimer(60, display);
+  // startTimer(gameDuration, display = $('#timer'))
